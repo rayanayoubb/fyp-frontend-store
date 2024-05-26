c75bfe3811f6898453d4609d29f9e334de56d916
rayanayoubb <12133216@students.liu.edu.lb>
2024-05-27 02:09:31 +0300

    Rayan6

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -101,7 +101,7 @@ const Footer = () => {
                 <path
                   d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
               </svg>
-               Lebanese,Beirut 
+               Lebanese,Beirutt 
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
